cd7d4ea9b744a3118571b6ce25332d4b971e425d
Khurram Ali <khurramcali@gmail.com>
2024-11-21 00:46:35 -0500

    lesson-2 init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,49 @@
-console.log("Hello Prisma");
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  //   const users = await prisma.user.findMany();
+  //   console.log(users);
+  //
+  // Create a user
+  //   const newUser = await prisma.user.create({
+  //     data: {
+  //       email: "n.uzumaki@example.com",
+  //     },
+  //   });
+  //   console.log(newUser);
+  //
+  // Create a user with a name
+  //   const newUser2 = await prisma.user.create({
+  //     data: {
+  //       email: "s.uchiha@example.com",
+  //       name: "Sasuke Uchiha",
+  //     },
+  //   });
+  //   console.log(newUser2);
+  //
+  // Create many users
+  //   const manyNewUsers = await prisma.user.createMany({
+  //     data: [
+  //       {
+  //         email: "s.haruno@example.com",
+  //       },
+  //       {
+  //         email: "i.uchiha@example.com",
+  //         name: "Itachi Uchiha",
+  //       },
+  //     ],
+  //   });
+  //   console.log(manyNewUsers);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (err) => {
+    console.error(err);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
