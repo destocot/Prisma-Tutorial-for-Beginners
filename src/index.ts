7fd900d0e0152a6508176a8f82697298f93f6292
Khurram Ali <khurramcali@gmail.com>
2024-11-21 00:58:44 -0500

    lesson-4 init

--- src/index.ts
+++ src/index.ts
@@ -3,30 +3,16 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  //   const users = await prisma.user.findMany();
-  //   console.log(users);
-  //
-  //   Find first user
-  //   const user1 = await prisma.user.findFirst();
-  //   console.log(user1);
-  //
-  //   Find user by id
-  //   const uniqueUser1 = await prisma.user.findUnique({
-  //     where: { id: 2 },
-  //   });
-  //   console.log(uniqueUser1);
-  //
-  //   Find user by email
-  //   const uniqueUser2 = await prisma.user.findUnique({
-  //     where: { email: "s.uchiha@example.com" },
-  //   });
-  //   console.log(uniqueUser2);
-  //
-  // Find first user with name ends with Uchiha
-  //   const user2 = await prisma.user.findFirst({
-  //     where: { name: { endsWith: "Uchiha" } },
-  //   });
-  //   console.log(user2);
+  // npx prisma studio
+  // view
+  // create
+  // update
+  // delete
+  // filter
+  // fields
+
+  const users = await prisma.user.findMany();
+  console.log(users);
 }
 
 main()
