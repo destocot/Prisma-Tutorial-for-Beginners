abdce68ed318e6bc5dfee19f39ca33e5be173fce
Khurram Ali <khurramcali@gmail.com>
2024-11-25 01:49:41 -0500

    lesson-11 init

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,87 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function main() {}
+async function main() {
+  // gt: greater than | Filter Posts Based on Views
+  // const popularPosts = await prisma.post.findMany({
+  //   where: {
+  //     views: {
+  //       gt: 500,
+  //     },
+  //   },
+  // });
+  // console.log("# of posts with more than 500 views", popularPosts.length);
+  //
+  // startsWith: starts with | Filter Posts Based on Title
+  // const posts = await prisma.post.findMany({
+  //   where: {
+  //     title: {
+  //       startsWith: "p",
+  //     },
+  //   },
+  // });
+  // console.log("# of posts starting with 'P'", posts.length);
+  //
+  // Filter Posts with Related User Information
+  // const userPosts = await prisma.post.findMany({
+  //   where: {
+  //     user: {
+  //       email: {
+  //         endsWith: "hotmail.com",
+  //       },
+  //     },
+  //   },
+  // });
+  // console.log("# of posts by users with hotmail email", userPosts.length);
+  //
+  // Filter Users Based on Published Posts
+  // const usersWithPublishedPosts = await prisma.user.findMany({
+  //   where: {
+  //     posts: {
+  //       some: {
+  //         isPublished: true,
+  //         views: {
+  //           lt: 100,
+  //         },
+  //       },
+  //     },
+  //   },
+  // });
+  // console.log(
+  //   "# of users with published posts",
+  //   usersWithPublishedPosts.length
+  // );
+  // AND - Logical Operator
+  const posts = await prisma.post.findMany({
+    where: {
+      AND: [
+        {
+          isPublished: true,
+        },
+        {
+          user: {
+            email: {
+              endsWith: "gmail.com",
+            },
+          },
+        },
+        {
+          views: {
+            gte: 800,
+          },
+        },
+      ],
+    },
+    select: {
+      title: true,
+      views: true,
+      isPublished: true,
+      user: { select: { email: true } },
+    },
+  });
+  console.log("# of posts that meet the criteria", posts.length);
+  console.log(posts);
+}
 
 main()
   .then(async () => {
