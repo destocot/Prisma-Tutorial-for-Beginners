f770196383de57f4c8653b746ca331ddb58432e7
Khurram Ali <khurramcali@gmail.com>
2024-11-25 00:03:19 -0500

    lesson-5 init

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,41 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  // npx prisma studio
-  // view
-  // create
-  // update
-  // delete
-  // filter
-  // fields
-
-  const users = await prisma.user.findMany();
-  console.log(users);
+  // View data going forward with Prisma Studio
+  //
+  // Update a user
+  // const updatedUser = await prisma.user.update({
+  //   where: {
+  //     id: 5,
+  //   },
+  //   data: {
+  //     name: "Rock Lee Updated",
+  //   },
+  // });
+  // console.log(updatedUser);
+  //
+  // Update many users
+  // const manyUpdatedUsers = await prisma.user.updateMany({
+  //   data: {
+  //     name: "REDACTED",
+  //   },
+  // });
+  // console.log(manyUpdatedUsers);
+  //
+  // Upsert a user
+  // const upsertedUser = await prisma.user.upsert({
+  //   where: {
+  //     email: "s.nara@example.com",
+  //   },
+  //   create: {
+  //     email: "s.nara@example.com",
+  //     name: "Shikamaru Nara",
+  //   },
+  //   update: {
+  //     name: "Shikamaru Nara Updated",
+  //   },
+  // });
+  // console.log(upsertedUser);
 }
 
 main()
